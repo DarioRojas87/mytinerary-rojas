f157096e08ea001ea3ee2f8bc9b9d7e22bdf8d83
Dario@Rojas <dariourojas07@gmail.com>
2021-08-05 23:38:05 -0300

    maquetando

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Main from "../components/Main";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="contenedor">
         <Header />
-        <div className="contenidoHome">
-          <Main
-            breed="German Shepherd"
-            weight1="66 pounds"
-            height1="1,64 foots"
-            coatColor="black and tan"
-          />
-        </div>
+        <div className="contenidoHome"></div>
         <Footer />
       </div>
     );
